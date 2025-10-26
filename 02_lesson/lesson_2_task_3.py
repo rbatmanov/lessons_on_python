986b22e3c3339fff97729ab6cf3ce9231bbf141f
rbatmanov <rbatmanov@mail.ru>
2025-10-26 22:11:58 +0300

    Revert "create branch"

--- 02_lesson/lesson_2_task_3.py
+++
@@ -1,8 +0,0 @@
-import math
-def square(side_length):
-    side_length_rounded = math.ceil(side_length)
-    area = side_length_rounded ** 2
-    return area
-side = float(input("Введите сторону квадрата: "))
-area = square(side)
-print(f"Площадь квадрата со стороной {side} равна {area}")
