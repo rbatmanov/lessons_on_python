5b20dbd8d1f1b7c211bd5d9706ce0e8cdc7690e5
unknown <rbatmanov@mail.ru>
2025-11-11 21:35:16 +0300

    lesson6

--- 06_lesson/lesson06_task2.py
+++ 06_lesson/lesson06_task2.py
@@ -1,4 +1,3 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
--- 06_lesson/time.py
+++
@@ -1,1 +0,0 @@
-def sleep(30)
